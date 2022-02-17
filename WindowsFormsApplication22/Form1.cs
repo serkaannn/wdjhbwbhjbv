623ec53a307fb220ab626834473c900d7ef96abf
serkan narin <99245202+serkaannn@users.noreply.github.com>
2022-02-17 09:59:32 +0300

    Add files via upload

---
+++ WindowsFormsApplication22/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int say1,say2, toplamö;
+            say1 = Convert.ToInt32(textBox1.Text);
+            say2 = Convert.ToInt32(textBox2.Text);
+            toplamö = 0;
+            while (say1 <= say2)
+            {
+                toplamö = say1 + toplamö;
+                say1++;
+            }
+            label1.Text="sayıların toplamı"+toplamö.ToString();
+        }
+    }
+}
